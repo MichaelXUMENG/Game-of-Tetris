4301be3ec07c257072c5bcb05fcfb11edc827771
Meng Xu <xumengmichael@gmail.com>
2014-09-14 21:43:27 -0500

    Good appearance
    Solve the label and button issue: they have to be put together with the
    painting area in the same layer

--- src/gameTetris.java
+++ src/gameTetris.java
@@ -4,7 +4,6 @@ import java.awt.event.*;
 
 public class gameTetris extends Frame{
 
-	functionArea func;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -24,22 +23,32 @@ public class gameTetris extends Frame{
 		int x = screenWidth/8;
 		int y = screenHeight/16;
 		
-		setSize(screenWidth/2,screenHeight*7/8);
+		setSize(screenWidth*3/7,screenHeight*6/8);
 		this.setLocation(x, y);
 		
-		func = new functionArea();
 		
-		add(func);
-//		add(new mainArea());
+		add(new mainArea());
 		show();
 	}
 
 }
 
-class mainArea extends Canvas{
+/************
+ * To show the mainArea with the painting area and labels and buttons together,
+ * All the elements must be put in the same layer
+ * ****************************************/
+class mainArea extends Panel{
+	
+	//declare the button and labels
+	public Button quit; 
+	public Label level, line, score, level_data, line_data, score_data;
+	
+	//declare the variables used to calculate the coordinates
 	int centerX, centerY;
 	float ris = 0.95F;
-	float pixelSize, width, height, rWidth = 100.0F, rHeight = 200.0F;
+	float pixelSize, width, height, rWidth = 100.0F, rHeight = 130.0F;
+	
+	int PX, PY, SX, SY;
 	
 	void initial(){
 		Dimension d = this.getSize();
@@ -54,15 +63,62 @@ class mainArea extends Canvas{
 	int iX(float x){return Math.round(centerX + x/pixelSize);}
 	int iY(float y){return Math.round(centerY - y/pixelSize);}
 	
+	mainArea(){
+		this.setLayout(null);
+		
+		//label 1 show the level of game
+		level = new Label("Level: ");
+		level_data = new Label("1");
+		
+		//label 2 shows the lines of game
+		line = new Label("Lines: ");
+		line_data = new Label("0");
+		
+		//label 3 shows the score of game
+		score = new Label("Score: ");
+		score_data = new Label("0");
+		
+		//quit button
+		quit = new Button("QUIT");
+		
+		this.add(level);
+		this.add(level_data);
+		this.add(line);
+		this.add(line_data);
+		this.add(score);
+		this.add(score_data);
+		this.add(quit);
+	}
+	
 	public void paint(Graphics g){
 		initial();
 		g.setColor(Color.white);
-		g.fillRect( iX(-rWidth/2) , iY(rHeight*ris/2), Math.round(width / pixelSize) , Math.round(height/pixelSize));
+		
+		//create the big white area
+		PX=iX(-rWidth/2); PY=iY(rHeight/2 - 5); SX= Math.round(width / pixelSize); SY=Math.round(height/pixelSize);
+		g.fillRect( PX , PY, SX , SY);
+		
+		//create the small area
 		width = rWidth/5; height = width/2;
-		g.fillRect(iX(rWidth/5), iY(rHeight*ris/2), Math.round(width/pixelSize) , Math.round(height/pixelSize));
+		PX=iX(rWidth/5); PY=iY(rHeight/2 - 5); SX= Math.round(width / pixelSize); SY=Math.round(height/pixelSize);
+		g.fillRect(PX, PY, SX , SY);
+		
+		
+		level.setBounds(PX,iY(rHeight/2 - 2*height - 10),SX , SY);
+		line.setBounds(PX,iY(rHeight/2 - 4*height - 10),SX , SY);
+		score.setBounds(PX,iY(rHeight/2 - 6*height - 10),SX , SY);
+		
+		quit.setBounds(PX,iY(rHeight/2 - 8*height - 10),SX , SY);
+		
+		PX = iX(rWidth/5 + width);
+		level_data.setBounds(PX,iY(rHeight/2 - 2*height - 10),SX , SY);
+		line_data.setBounds(PX,iY(rHeight/2 - 4*height - 10),SX , SY);
+		score_data.setBounds(PX, iY(rHeight/2 - 6*height - 10), SX , SY);
 	}
 }
 
+
+/*
 class functionArea extends Panel{
 	public Button quit;
 	public Label level, line, score, level_data, line_data, score_data;
@@ -113,4 +169,4 @@ class functionArea extends Panel{
 //		score_data.setBounds(iX(rWidth/5), iY(rHeight*ris/2), Math.round(width/pixelSize) , Math.round(height/pixelSize));
 		
 	}
-}
+}   */
