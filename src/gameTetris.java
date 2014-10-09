df63c0bdac71d2cc235bd397069c98f32c180636
Meng Xu <xumengmichael@gmail.com>
2014-10-09 10:21:32 -0500

    Oct 9 10:21
    should execute the moving

--- src/gameTetris.java
+++ src/gameTetris.java
@@ -1,9 +1,14 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Random;
 
 
-public class gameTetris extends Frame{
+public class gameTetris extends Frame implements MouseListener{
 
+	int current, next;
+	boolean start = true;
+	boolean round = false;
+	mainArea mA;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -12,6 +17,7 @@ public class gameTetris extends Frame{
 	
 	gameTetris(){
 		super("Game of Tetris");
+		mA = new mainArea();
 		//add the close window action
 		addWindowListener(new WindowAdapter()
 		   {public void windowClosing(WindowEvent e){System.exit(0);}});
@@ -28,9 +34,69 @@ public class gameTetris extends Frame{
 		this.setLocation(x, y);
 		
 		
-		add(new mainArea());
-		show();
+		add(mA);
+		this.setVisible(true);
+		this.addMouseListener(this);
+
+		next = new Random().nextInt(6);
+		current = next;
+		
+		while(true){
+			while(start){
+				current = next;
+				next = new Random().nextInt(6);
+				mA.setShapes(current, next);
+				round = true;
+				while(round){
+					try {
+						mA.fallDown();
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+		
 	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getButton() == e.BUTTON1){
+			mA.leftClick();
+		}
+		
+		else if(e.getButton() == e.BUTTON3){
+			mA.rightClick();
+		}
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
 
 }
 
@@ -38,7 +104,7 @@ public class gameTetris extends Frame{
  * To show the mainArea with the painting area and labels and buttons together,
  * All the elements must be put in the same layer
  * ****************************************/
-class mainArea extends Panel implements MouseMotionListener, ActionListener{
+class mainArea extends Panel implements MouseMotionListener, ActionListener, MouseListener{
 
 	
 	//declare the button and labels
@@ -48,7 +114,7 @@ class mainArea extends Panel implements MouseMotionListener, ActionListener{
 	//declare the variables used to calculate the coordinates
 	int centerX, centerY; // the central pixel's position
 	float pixelSize, width, height, //width and height is the logical width and height of my elements
-	rWidth = 100.0F, rHeight = 130.0F, 
+	rWidth = 100.0F, rHeight = 130.0F,     //used to calculate the pixel size
 			sqSize; //logical size of a square
 	
 	int PX, PY, SX, SY, Es; //the actual position of pixel: PX,PY are position; SX,SY are actual size, and Es is the actual size of a square.
@@ -56,7 +122,11 @@ class mainArea extends Panel implements MouseMotionListener, ActionListener{
 	int[][] sqPositionY = new int[20][10];
 
 	drawElements dE = new drawElements( );
-
+	
+	int current, next;
+	boolean start = true;
+	boolean round = true;
+	int row = 0, collum = 3;
 
 
 
@@ -104,7 +174,12 @@ class mainArea extends Panel implements MouseMotionListener, ActionListener{
 		this.add(quit);
 		
 		this.addMouseMotionListener(this);
+//		this.addMouseListener(this);
+		
+		
+
 	}
+	
 	
 	public void paint(Graphics g){
 		super.paint(g);
@@ -113,7 +188,6 @@ class mainArea extends Panel implements MouseMotionListener, ActionListener{
 		
 		//create the big white area
 		PX=iX(-rWidth/2); PY=iY(rHeight/2 - 5); //get the position left-top corner of the big area
-		SX= Math.round(width / pixelSize); SY=Math.round(height/pixelSize); //get the size of the big area
 		dE.fillArea(g, Es, PX, PY, 10, 20);
 		
 		//store all the position into the array
@@ -150,7 +224,7 @@ class mainArea extends Panel implements MouseMotionListener, ActionListener{
 		int ePX, ePY;
 		
 		ePX = iX(rWidth/5 + (width- 4*sqSize)/2);
-		ePY = iY((rHeight/2 - 5)- (height-sqSize)/2);
+		ePY = iY((rHeight/2 - 10)- (height-sqSize)/2);
 		
 /*		g.setColor(Color.black);
 		for(int i = 0; i<4; i++){
@@ -161,13 +235,70 @@ class mainArea extends Panel implements MouseMotionListener, ActionListener{
 			g.fillRect(ePX+i*Es+1, ePY+1, Es-1, Es-1);
 		}
 		*/
+		switch(next){
+		case 0:{
+			dE.drawLeftZShape(g, Es, ePX, ePY,0);
+			break;
+		}
+		case 1:{
+			dE.drawRightZShape(g, Es, ePX, ePY,0);
+			break;
+		}
+		case 2:{
+			dE.drawLeftLShape(g, Es, ePX, ePY,0);
+			break;
+		}
+		case 3:{
+			dE.drawRightLShape(g, Es, ePX, ePY,0);
+			break;
+		}
+		case 4:{
+			dE.drawCube(g, Es, ePX, ePY,0);
+			break;
+		}
+		case 5:{
+			dE.drawHillShape(g, Es, ePX, ePY,0);
+			break;
+		}
+		case 6:{
+			dE.drawLine(g, Es, ePX, ePY,0);
+			break;
+		}
 		
-		dE.drawZShape(g, Es, ePX, ePY);
+		}
 		
-		dE.drawLShape(g, Es, sqPositionX[19][7], sqPositionY[19][7]);
-		dE.drawZShape(g, Es, sqPositionX[19][5], sqPositionY[19][5]);
-		dE.drawCube(g, Es, sqPositionX[5][5], sqPositionY[5][5]);
-		dE.drawLine(g, Es, sqPositionX[10][5], sqPositionY[10][5]);
+		
+		switch(current){
+		case 0:{
+			dE.drawLeftZShape(g, Es, sqPositionX[row][collum], sqPositionY[row][collum],0);
+			break;
+		}
+		case 1:{
+			dE.drawRightZShape(g, Es, sqPositionX[row][collum], sqPositionY[row][collum],0);
+			break;
+		}
+		case 2:{
+			dE.drawLeftLShape(g, Es, sqPositionX[row][collum], sqPositionY[row][collum],0);
+			break;
+		}
+		case 3:{
+			dE.drawRightLShape(g, Es, sqPositionX[row][collum], sqPositionY[row][collum],0);
+			break;
+		}
+		case 4:{
+			dE.drawCube(g, Es, sqPositionX[row][collum], sqPositionY[row][collum],0);
+			break;
+		}
+		case 5:{
+			dE.drawHillShape(g, Es, sqPositionX[row][collum], sqPositionY[row][collum],0);
+			break;
+		}
+		case 6:{
+			dE.drawLine(g, Es, sqPositionX[row][collum], sqPositionY[row][collum],0);
+			break;
+		}
+		
+		}
 	}
 	
 
@@ -205,7 +336,6 @@ class mainArea extends Panel implements MouseMotionListener, ActionListener{
 			else{
 				if(!inside){
 					inside = false;
-//					dE.clearSring(g, sqPositionX[7][2], sqPositionY[7][2], "PAUSE");
 					repaint();
 				}
 			}
@@ -213,16 +343,81 @@ class mainArea extends Panel implements MouseMotionListener, ActionListener{
 		else{
 			if(!inside){
 				inside = false;
-//				dE.clearSring(g, sqPositionX[7][2], sqPositionY[7][2], "PAUSE");
 				repaint();
 			}
 		}
 		
 	}
+	
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getButton() == e.BUTTON1){
+			if(dE.getLeft()>sqPositionX[0][0]){
+				collum--;
+				repaint();
+			}
+		}
+		
+		else if(e.getButton() == e.BUTTON3){
+			if(dE.getRight() < sqPositionX[0][9]){
+				collum++;
+				repaint();
+			}
+		}
+	}
 
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 	
+	/**************************************************
+	 * Here are some functions that will be used by mainArea
+	 * *******************************************/
+	void leftClick(){
+		if(dE.getLeft()>sqPositionX[0][0]){
+			collum--;
+			repaint();
+		}
+	}
 	
+	void rightClick(){
+		if(dE.getRight() < sqPositionX[0][9]){
+			collum++;
+			repaint();
+		}
+	}
+	
+	void fallDown(){
+		repaint();
+		row--;
+	}
+	
+	void setShapes(int big, int small){
+		this.current = big;
+		this.next = small;
+	}
 }
 
 
@@ -231,20 +426,62 @@ class mainArea extends Panel implements MouseMotionListener, ActionListener{
  * This is the Class for elements drawing including the line, cube, LShape and ZShape
  * ***************************************/
 class drawElements{
-
+/* size is the size of one cube element
+ * Px and Py must be the coordinate of turning point of any shape!!!!
+ * This means that any shape is drawn from bottom, which could result in beyond the top of main area;
+ * not the bottom of the main area!!!
+*/
+	int leftMost, rightMost, bottom;
 	
-	void drawLine(Graphics g, int size, int Px, int Py){
-		g.setColor(Color.black);
-		for(int i = 0; i<4; i++){
-			g.drawRect(Px+i*size, Py, size, size);
+	void drawLine(Graphics g, int size, int Px, int Py, int index){
+		int shape;
+		shape = index%2;
+		switch(shape){
+		case 0: {
+			leftMost = Px; rightMost = Px + 3*size; bottom = Py;
+			g.setColor(Color.black);
+			for(int i = 0; i<3; i++){
+				g.drawRect(Px+i*size, Py, size, size);
+				if(i == 1){
+					g.drawRect(Px-size, Py, size, size);
+				}
+			}
+			
+			g.setColor(Color.red);
+			for(int i = 0; i<3; i++){
+				g.fillRect(Px+i*size+1, Py+1, size-1, size-1);
+				if(i == 1){
+					g.fillRect(Px-size+1, Py+1, size-1, size-1);
+				}
+			}
+			break;
 		}
-		g.setColor(Color.red);
-		for(int i = 0; i<4; i++){
-			g.fillRect(Px+i*size+1, Py+1, size-1, size-1);
-		}	
+		case 1:{
+			leftMost = Px; rightMost = Px; bottom = Py+3*size;
+			g.setColor(Color.black);
+			for(int i = 0; i<3; i++){
+				g.drawRect(Px, Py-i*size, size, size);
+				if(i == 1){
+					g.drawRect(Px, Py+size, size, size);
+				}
+			}
+			
+			g.setColor(Color.red);
+			for(int i = 0; i<3; i++){
+				g.fillRect(Px+1, Py-i*size+1, size-1, size-1);
+				if(i == 1){
+					g.fillRect(Px+1, Py+size+1, size-1, size-1);
+				}
+			}
+			break;
+		}
+		
+		}
+			
 	}
 	
-	void drawCube(Graphics g, int size, int Px, int Py){
+	void drawCube(Graphics g, int size, int Px, int Py, int index){
+		leftMost = Px; rightMost = Px+size; bottom = Py+size;
 		g.setColor(Color.black);
 		for(int i = 0; i<2; i++){
 			for(int j = 0; j<2; j++)
@@ -257,31 +494,291 @@ class drawElements{
 		}
 	}
 	
-	void drawLShape(Graphics g, int size, int Px, int Py){
-		g.setColor(Color.black);
-		for(int i = 0; i<3; i++){
-			g.drawRect(Px+i*size, Py, size, size);
+	void drawRightLShape(Graphics g, int size, int Px, int Py, int index){
+		int shape;
+		shape = index%4;
+		switch(shape){
+		case 0:{
+			leftMost = Px-size; rightMost = Px + size; bottom = Py;
+			g.setColor(Color.black);
+			g.drawRect(Px, Py, size, size);
+			g.drawRect(Px-size, Py, size, size);
+			g.drawRect(Px+size, Py, size, size);
+			g.drawRect(Px+size, Py-size, size, size);
+			
+			g.setColor(Color.blue);
+			g.fillRect(Px+1, Py+1, size-1, size-1);
+			g.fillRect(Px-size+1, Py+1, size-1, size-1);
+			g.fillRect(Px+size+1, Py+1, size-1, size-1);
+			g.fillRect(Px+size+1, Py-size+1, size-1, size-1);
+			break;
 		}
-		g.drawRect(Px+2*size, Py-size, size, size);
-		g.setColor(Color.blue);
-		for(int i = 0; i<3; i++){
-			g.fillRect(Px+i*size+1, Py+1, size-1, size-1);
+		case 1:{
+			leftMost = Px; rightMost = Px + size; bottom = Py+size;
+			g.setColor(Color.black);
+			g.drawRect(Px, Py, size, size);
+			g.drawRect(Px, Py-size, size, size);
+			g.drawRect(Px, Py+size, size, size);
+			g.drawRect(Px+size, Py+size, size, size);
+			
+			g.setColor(Color.blue);
+			g.fillRect(Px+1, Py+1, size-1, size-1);
+			g.fillRect(Px+1, Py-size+1, size-1, size-1);
+			g.fillRect(Px+1, Py+size+1, size-1, size-1);
+			g.fillRect(Px+size+1, Py+size+1, size-1, size-1);
+			break;
 		}
-		g.fillRect(Px+2*size+1, Py-size+1, size-1, size-1);
+		case 2:{
+			leftMost = Px-size; rightMost = Px + size; bottom = Py+size;
+			g.setColor(Color.black);
+			g.drawRect(Px, Py, size, size);
+			g.drawRect(Px+size, Py, size, size);
+			g.drawRect(Px-size, Py, size, size);
+			g.drawRect(Px-size, Py+size, size, size);
+			
+			g.setColor(Color.blue);
+			g.fillRect(Px+1, Py+1, size-1, size-1);
+			g.fillRect(Px+size+1, Py+1, size-1, size-1);
+			g.fillRect(Px-size+1, Py+1, size-1, size-1);
+			g.fillRect(Px-size+1, Py+size+1, size-1, size-1);
+			break;
+		}
+		case 3:{
+			leftMost = Px-size; rightMost = Px; bottom = Py+size;
+			g.setColor(Color.black);
+			g.drawRect(Px, Py, size, size);
+			g.drawRect(Px, Py+size, size, size);
+			g.drawRect(Px, Py-size, size, size);
+			g.drawRect(Px-size, Py-size, size, size);
+			
+			g.setColor(Color.blue);
+			g.fillRect(Px+1, Py+1, size-1, size-1);
+			g.fillRect(Px+1, Py+size+1, size-1, size-1);
+			g.fillRect(Px+1, Py-size+1, size-1, size-1);
+			g.fillRect(Px-size+1, Py-size+1, size-1, size-1);
+			break;
+		}
+		
+		}
+		
 	}
 	
-	void drawZShape(Graphics g, int size, int Px, int Py){
-		g.setColor(Color.black);
-		for(int i = 0; i<2; i++){
-			for(int j = 0; j<2; j++)
-				g.drawRect(Px+(j+i)*size, Py-i*size, size, size);
+	void drawLeftLShape(Graphics g, int size, int Px, int Py, int index){
+		int shape;
+		shape = index%4;
+		switch(shape){
+		case 0:{
+			leftMost = Px-size; rightMost = Px + size; bottom = Py;
+			g.setColor(Color.black);
+			g.drawRect(Px, Py, size, size);
+			g.drawRect(Px+size, Py, size, size);
+			g.drawRect(Px-size, Py, size, size);
+			g.drawRect(Px-size, Py-size, size, size);
+			
+			g.setColor(Color.gray);
+			g.fillRect(Px+1, Py+1, size-1, size-1);
+			g.fillRect(Px+size+1, Py+1, size-1, size-1);
+			g.fillRect(Px-size+1, Py+1, size-1, size-1);
+			g.fillRect(Px-size+1, Py-size+1, size-1, size-1);
+			break;
 		}
-		g.setColor(Color.yellow);
-		for(int i = 0; i<2; i++){
-			for(int j = 0; j<2; j++)
-				g.fillRect(Px+(j+i)*size+1, Py-i*size+1, size-1, size-1);
+		case 1:{
+			leftMost = Px; rightMost = Px + size; bottom = Py+size;
+			g.setColor(Color.black);
+			g.drawRect(Px, Py, size, size);
+			g.drawRect(Px, Py+size, size, size);
+			g.drawRect(Px, Py-size, size, size);
+			g.drawRect(Px+size, Py-size, size, size);
+			
+			g.setColor(Color.gray);
+			g.fillRect(Px+1, Py+1, size-1, size-1);
+			g.fillRect(Px+1, Py+size+1, size-1, size-1);
+			g.fillRect(Px+1, Py-size+1, size-1, size-1);
+			g.fillRect(Px+size+1, Py-size+1, size-1, size-1);
+			break;
+		}
+		case 2:{
+			leftMost = Px-size; rightMost = Px + size; bottom = Py+size;
+			g.setColor(Color.black);
+			g.drawRect(Px, Py, size, size);
+			g.drawRect(Px-size, Py, size, size);
+			g.drawRect(Px+size, Py, size, size);
+			g.drawRect(Px+size, Py+size, size, size);
+			
+			g.setColor(Color.gray);
+			g.fillRect(Px+1, Py+1, size-1, size-1);
+			g.fillRect(Px-size+1, Py+1, size-1, size-1);
+			g.fillRect(Px+size+1, Py+1, size-1, size-1);
+			g.fillRect(Px+size+1, Py+size+1, size-1, size-1);
+			break;
+		}
+		case 3:{
+			leftMost = Px-size; rightMost = Px; bottom = Py+size;
+			g.setColor(Color.black);
+			g.drawRect(Px, Py, size, size);
+			g.drawRect(Px, Py-size, size, size);
+			g.drawRect(Px, Py+size, size, size);
+			g.drawRect(Px-size, Py+size, size, size);
+			
+			g.setColor(Color.gray);
+			g.fillRect(Px+1, Py+1, size-1, size-1);
+			g.fillRect(Px+1, Py-size+1, size-1, size-1);
+			g.fillRect(Px+1, Py+size+1, size-1, size-1);
+			g.fillRect(Px-size+1, Py+size+1, size-1, size-1);
+			break;
+		}
+		
+		}
+		
+	}
+	
+	void drawLeftZShape(Graphics g, int size, int Px, int Py, int index){
+		int shape;
+		shape = index%2;
+		switch(shape){
+		case 0:{
+			leftMost = Px-size; rightMost = Px +size; bottom = Py;
+			g.setColor(Color.black);
+			g.drawRect(Px, Py, size, size);
+			g.drawRect(Px-size, Py, size, size);
+			g.drawRect(Px, Py-size, size, size);
+			g.drawRect(Px+size, Py-size, size, size);
+			
+			g.setColor(Color.yellow);
+			g.fillRect(Px+1, Py+1, size-1, size-1);
+			g.fillRect(Px-size+1, Py+1, size-1, size-1);
+			g.fillRect(Px+1, Py-size+1, size-1, size-1);
+			g.fillRect(Px+size+1, Py-size+1, size-1, size-1);
+			break;
+		}
+		case 1:{
+			leftMost = Px; rightMost = Px + size; bottom = Py+size;
+			g.setColor(Color.black);
+			g.drawRect(Px, Py, size, size);
+			g.drawRect(Px, Py-size, size, size);
+			g.drawRect(Px+size, Py, size, size);
+			g.drawRect(Px+size, Py+size, size, size);
+			
+			g.setColor(Color.yellow);
+			g.fillRect(Px+1, Py+1, size-1, size-1);
+			g.fillRect(Px+1, Py-size+1, size-1, size-1);
+			g.fillRect(Px+size+1, Py+1, size-1, size-1);
+			g.fillRect(Px+size+1, Py+size+1, size-1, size-1);
+			break;
+		}
+		
 		}
 	}
+	
+	void drawRightZShape(Graphics g, int size, int Px, int Py, int index){
+		int shape;
+		shape = index%2;
+		switch(shape){
+		case 0:{
+			leftMost = Px-size; rightMost = Px + size; bottom = Py;
+			g.setColor(Color.black);
+			g.drawRect(Px, Py, size, size);
+			g.drawRect(Px+size, Py, size, size);
+			g.drawRect(Px, Py-size, size, size);
+			g.drawRect(Px-size, Py-size, size, size);
+			
+			g.setColor(Color.orange);
+			g.fillRect(Px+1, Py+1, size-1, size-1);
+			g.fillRect(Px+size+1, Py+1, size-1, size-1);
+			g.fillRect(Px+1, Py-size+1, size-1, size-1);
+			g.fillRect(Px-size+1, Py-size+1, size-1, size-1);
+			break;
+		}
+		case 1:{
+			leftMost = Px; rightMost = Px + size; bottom = Py+size;
+			g.setColor(Color.black);
+			g.drawRect(Px, Py, size, size);
+			g.drawRect(Px, Py-size, size, size);
+			g.drawRect(Px-size, Py, size, size);
+			g.drawRect(Px-size, Py+size, size, size);
+			
+			g.setColor(Color.orange);
+			g.fillRect(Px+1, Py+1, size-1, size-1);
+			g.fillRect(Px+1, Py-size+1, size-1, size-1);
+			g.fillRect(Px-size+1, Py+1, size-1, size-1);
+			g.fillRect(Px-size+1, Py+size+1, size-1, size-1);
+			break;
+		}
+		
+		}
+		
+	}
+	
+	void drawHillShape(Graphics g, int size, int Px, int Py, int index){
+		int shape;
+		shape = index%4;
+		switch(shape){
+		case 0:{
+			leftMost = Px-size; rightMost = Px + size; bottom = Py;
+			g.setColor(Color.black);
+			g.drawRect(Px, Py, size, size);
+			g.drawRect(Px +size, Py, size, size);
+			g.drawRect(Px -size, Py, size, size);
+			g.drawRect(Px, Py - size, size, size);
+			
+			g.setColor(Color.pink);
+			g.fillRect(Px +1, Py+1, size-1, size-1);
+			g.fillRect(Px + size +1, Py+1, size-1, size-1);
+			g.fillRect(Px -size +1, Py+1, size-1, size-1);
+			g.fillRect(Px +1, Py- size +1, size-1, size-1);
+			break;
+		}
+		case 1:{
+			leftMost = Px; rightMost = Px + size; bottom = Py+size;
+			g.setColor(Color.black);
+			g.drawRect(Px, Py, size, size);
+			g.drawRect(Px, Py-size, size, size);
+			g.drawRect(Px, Py+size, size, size);
+			g.drawRect(Px+size, Py, size, size);
+			
+			g.setColor(Color.pink);
+			g.fillRect(Px +1, Py+1, size-1, size-1);
+			g.fillRect(Px+1, Py-size+1, size-1, size-1);
+			g.fillRect(Px+1, Py+size+1, size-1, size-1);
+			g.fillRect(Px+size+1, Py+1, size-1, size-1);
+			break;
+		}
+		case 2:{
+			leftMost = Px-size; rightMost = Px + size; bottom = Py+size;
+			g.setColor(Color.black);
+			g.drawRect(Px, Py, size, size);
+			g.drawRect(Px +size, Py, size, size);
+			g.drawRect(Px -size, Py, size, size);
+			g.drawRect(Px, Py+size, size, size);
+			
+			g.setColor(Color.pink);
+			g.fillRect(Px +1, Py+1, size-1, size-1);
+			g.fillRect(Px + size +1, Py+1, size-1, size-1);
+			g.fillRect(Px -size +1, Py+1, size-1, size-1);
+			g.fillRect(Px +1, Py+size +1, size-1, size-1);
+			break;
+		}
+		case 3:{
+			leftMost = Px-size; rightMost = Px; bottom = Py+size;
+			g.setColor(Color.black);
+			g.drawRect(Px, Py, size, size);
+			g.drawRect(Px, Py-size, size, size);
+			g.drawRect(Px, Py+size, size, size);
+			g.drawRect(Px-size, Py, size, size);
+			
+			g.setColor(Color.pink);
+			g.fillRect(Px +1, Py+1, size-1, size-1);
+			g.fillRect(Px+1, Py-size+1, size-1, size-1);
+			g.fillRect(Px+1, Py+size+1, size-1, size-1);
+			g.fillRect(Px-size+1, Py+1, size-1, size-1);
+			break;
+		}
+		
+		}
+		
+	}
+	
+	
 	
 	void fillArea(Graphics g, int size, int Px, int Py, int length, int height){
 		g.setColor(Color.white);
@@ -305,5 +802,15 @@ class drawElements{
 		g.setFont(new Font("Times New Romen", Font.BOLD, 60));
 		g.drawString(s, Px+10, Py+55);
 		
+	}
+	
+	int getLeft(){
+		return leftMost;
+	}
+	int getRight(){
+		return rightMost;
+	}
+	int getBottom(){
+		return bottom;
 	}
 } 
